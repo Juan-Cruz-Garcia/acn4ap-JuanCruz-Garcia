e09fd20dbb860c830b87d77493d3033e7b2efc77
Juan Cruz <juan.garcia01@davinci.edu.ar>
2024-11-20 17:19:27 -0300

    vievs xml

---
+++ CodeGym/app/src/main/java/com/example/codegym/CrudEjercicosActivity.java
@@ -0,0 +1,14 @@
+package com.example.codegym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrudEjercicosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crud_ejercicos);
+    }
+}
