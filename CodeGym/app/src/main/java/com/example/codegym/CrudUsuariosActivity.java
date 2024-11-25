16dc6c4c1888e145d19d3878a31eeecf8c586343
Juan Cruz <juan.garcia01@davinci.edu.ar>
2024-11-25 11:40:32 -0300

    crud users

--- CodeGym/app/src/main/java/com/example/codegym/CrudUsuariosActivity.java
+++ CodeGym/app/src/main/java/com/example/codegym/CrudUsuariosActivity.java
@@ -1,13 +1,69 @@
 package com.example.codegym;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.codegym.adapter.UsuariosAdapter;
+import com.example.codegym.dao.FirebaseUsuarioDAO;
+import com.example.codegym.dto.UsuarioDTO;
+import com.example.codegym.listeners.OnItemsReceivedListener;
+
+import java.util.List;
 
 public class CrudUsuariosActivity extends AppCompatActivity {
+private TextView entrenadorTextView;
+private RecyclerView usuariosRecyclerView;
+private UsuariosAdapter usuariosAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crud_usuarios);
+
+        entrenadorTextView = findViewById(R.id.entrenadorTextView);
+        usuariosRecyclerView = findViewById(R.id.usuariosRecyclerView);
+        usuariosRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        obtenerUsuarios();
+    }
+
+    private void obtenerUsuarios() {
+        FirebaseUsuarioDAO usuarioDAO = new FirebaseUsuarioDAO();
+
+        usuarioDAO.obtenerTodo(new OnItemsReceivedListener<UsuarioDTO>() {
+            @Override
+            public void onItemsReceived(List<UsuarioDTO> usuarios) {
+                if (usuarios != null && !usuarios.isEmpty()) {
+                    usuariosAdapter = new UsuariosAdapter(
+                            usuarios,
+                            (usuario, position) -> { // Lógica para eliminar
+                                usuarioDAO.eliminar((UsuarioDTO) usuario);
+                                usuarios.remove(position);
+                                usuariosAdapter.notifyItemRemoved(position);
+                                usuariosAdapter.notifyItemRangeChanged(position, usuarios.size());
+                            },
+                            (usuario, position) -> { // Lógica para editar
+                                Intent intent = new Intent(CrudUsuariosActivity.this, ModificarUsuarioActivity.class);
+                                intent.putExtra("usuario",(UsuarioDTO) usuario); // Enviar el ejercicio para edición
+                                startActivity(intent);
+                            }
+                    );
+                    usuariosRecyclerView.setAdapter(usuariosAdapter);
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e("CrudUsuariosActivity", "Error al obtener Usuarios", e);
+
+            }
+
+        });
     }
 }
