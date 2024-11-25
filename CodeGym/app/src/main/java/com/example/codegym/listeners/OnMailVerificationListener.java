08a0fca7628089346db693e61b0eb5ad02e6e360
Juan Cruz <juan.garcia01@davinci.edu.ar>
2024-11-24 22:15:19 -0300

    new listeners

--- CodeGym/app/src/main/java/com/example/codegym/listeners/OnMailVerificationListener.java
+++ CodeGym/app/src/main/java/com/example/codegym/listeners/OnMailVerificationListener.java
@@ -2,6 +2,8 @@ package com.example.codegym.listeners;
 
 public interface OnMailVerificationListener {
     void onMailAvailable();
+
     void onMailAlreadyRegistered();
+
     void onError(Exception e);
 }
