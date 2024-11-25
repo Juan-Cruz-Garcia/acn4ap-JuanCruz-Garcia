6cc48258d7e16a6aa035d431cdeb214bf48cc87c
Juan Cruz <juan.garcia01@davinci.edu.ar>
2024-11-24 22:22:37 -0300

    idk

--- CodeGym/app/src/main/java/com/example/codegym/adapter/ExercisesAdapter.java
+++ CodeGym/app/src/main/java/com/example/codegym/adapter/ExercisesAdapter.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,6 +13,7 @@ import com.example.codegym.R;
 import com.example.codegym.dto.EjercicioDTO;
 
 import java.util.List;
+
 //Es un puente entre un RecyclerView y los datos a mostrar.
 public class ExercisesAdapter extends RecyclerView.Adapter<ExercisesAdapter.ExerciseViewHolder> {
     //lista donde se guardan los datos traidos de la base de datos
@@ -20,6 +22,7 @@ public class ExercisesAdapter extends RecyclerView.Adapter<ExercisesAdapter.Exer
     public ExercisesAdapter(List<EjercicioDTO> ejercicios) {
         this.ejercicios = ejercicios;
     }
+
     //agarra el xml base (activity_item) y lo convierte en una view manipulable
     @NonNull
     @Override
@@ -28,34 +31,37 @@ public class ExercisesAdapter extends RecyclerView.Adapter<ExercisesAdapter.Exer
                 .inflate(R.layout.activity_item, parent, false);
         return new ExerciseViewHolder(view);
     }
+
     //Asocia los datos del objeto EjercicioDTO a las vistas correspondientes.
     @Override
     public void onBindViewHolder(@NonNull ExerciseViewHolder holder, int position) {
         EjercicioDTO ejercicio = ejercicios.get(position);
         if (ejercicio != null && ejercicio.getNombre() != null) {
             holder.nombreTextView.setText(ejercicio.getNombre());
-                // Cambia el hint del EditText segun el tipo de ejercicio
-                if ("peso".equalsIgnoreCase(ejercicio.getTipo())) {
-                    holder.ingresoPesoEditText.setHint("Peso levantado");
-                } else if ("tiempo".equalsIgnoreCase(ejercicio.getTipo())) {
-                    holder.ingresoPesoEditText.setHint("Tiempo sostenido");
-                } else if ("repeticion".equalsIgnoreCase(ejercicio.getTipo())) {
-                    holder.ingresoPesoEditText.setHint("cantidad echa");
-                } else {
-                    holder.ingresoPesoEditText.setHint("Dato no especificado");
-                }
+            // Cambia el hint del EditText segun el tipo de ejercicio
+            if ("peso".equalsIgnoreCase(ejercicio.getTipo())) {
+                holder.ingresoPesoEditText.setHint("Peso levantado");
+            } else if ("tiempo".equalsIgnoreCase(ejercicio.getTipo())) {
+                holder.ingresoPesoEditText.setHint("Tiempo sostenido");
+            } else if ("repeticion".equalsIgnoreCase(ejercicio.getTipo())) {
+                holder.ingresoPesoEditText.setHint("cantidad echa");
+            } else {
+                holder.ingresoPesoEditText.setHint("Dato no especificado");
+            }
         }
 
     }   //devuelve el numero total de elementos en  la lista
+
     @Override
     public int getItemCount() {
-        return ejercicios != null ? ejercicios.size() : 0;
+        return ejercicios.size();
     }
 
     //llama a las variables a modificar
-    static class ExerciseViewHolder extends RecyclerView.ViewHolder {
-        TextView nombreTextView;
-        EditText ingresoPesoEditText;
+    public class ExerciseViewHolder extends RecyclerView.ViewHolder {
+        private TextView nombreTextView;
+        private EditText ingresoPesoEditText;
+
         public ExerciseViewHolder(@NonNull View itemView) {
             super(itemView);
             nombreTextView = itemView.findViewById(R.id.nombreEjercicioTextView);
