89cb9c44e9ce5f1d547cbc2b03d89263f95bfde7
Juan Cruz <juan.garcia01@davinci.edu.ar>
2024-11-25 09:49:37 -0300

    new button

--- CodeGym/app/src/main/java/com/example/codegym/panelAdminActivity.java
+++ CodeGym/app/src/main/java/com/example/codegym/panelAdminActivity.java
@@ -2,13 +2,28 @@ package com.example.codegym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class panelAdminActivity extends AppCompatActivity {
-
+    private Button PanelEjerciciosButton, PanelUsuariosButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel_admin);
+        PanelEjerciciosButton = findViewById(R.id.PanelEjerciciosButton);
+        PanelUsuariosButton = findViewById(R.id.PanelUsuariosButton);
+
+        PanelEjerciciosButton.setOnClickListener(View ->{
+            Intent intent = new Intent(panelAdminActivity.this,CrudEjercicosActivity.class);
+            startActivity(intent);
+        });
+        PanelUsuariosButton.setOnClickListener(View ->{
+            Intent intent = new Intent(panelAdminActivity.this, CrudUsuariosActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 }
